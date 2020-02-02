c883aac5e185ace483ec1a56017eaf91e38b2125
Guido Eco <will.deakin@crinstitute.org.uk>
2020-02-02 22:32:02 +0000

    add ERROR codes

--- bin/app/__init__.py
+++ bin/app/__init__.py
@@ -4,4 +4,4 @@ from os.path import dirname, abspath
 
 sys.path = sys.path + [dirname(abspath(__file__))]
 
-__all__ = ['check_missing_status', 'create_collection', 'delete_collection', 'delete_schema', 'error_solr', 'get_count', 'get_names', 'get_schema', 'ping_name', 'raw_query', 'set_schema', 'wait_for_success']
+__all__ = ['SOLRERR', 'HTTPError', 'check_missing_status', 'create_collection', 'delete_collection', 'type_error_solr', 'get_count', 'get_names', 'get_schema', 'ping_name', 'raw_query', 'set_schema', 'wait_for_success', 'get_solrmode']
