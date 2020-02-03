e9989bc416cd9e132bd2dd19a3c636f06dda0614
Will Deakin <will.deakin@crinstitute.org.uk>
2020-02-03 13:10:07 +0000

    delete local files and update run script

--- TPS/run.sh
+++ TPS/run.sh
@@ -3,7 +3,40 @@
 export PYTHONUNBUFFERED=1
 export PATH=${PATH}:../bin
 
-for FILE in output/*.jsonl
+count-documents.py _test_ 2> /dev/null
+if [ "$?" != "0" ]; then
+    start-cluster.sh
+    while true
+    do
+        count-documents.py _test_ 2> /dev/null
+        if [ "$?" = "0" ]; then
+            break
+        fi
+    done
+fi
+
+URL=https://networkrail.opendata.opentraintimes.com/mirror/tpsdata
+if [ ! -f file-list.txt ]; then
+    curl -s -L -G  ${URL}| \
+        htmltojson.py --stdout --depth 5 | \
+        jq -cr '.td? | select(.[1] | tonumber > 1024)? | .[0].a.value' > file-list.txt
+fi
+
+if [ ! -f XML_p.xml ]; then
+    FILENAME=$(tail -1 file-list.txt)
+    echo Download and uncompress ${FILENAME} to TPS_Data.tar.bz2
+    curl ${URL}/${FILENAME} -o TPS_Data.tar.bz2
+    tar jxvf TPS_Data.tar.bz2
+fi
+
+if [ ! -d data ]; then
+    echo Process XML_p.xml
+    mkdir data
+    xmltojson.py --path data --depth 1 --encoding cp437 XML_p.xml
+    #xml2json.py --path run --depth 1 --encoding cp437 XML_p.xml
+fi
+
+for FILE in data/*.jsonl
 do
     ID=$(basename ${FILE} | sed 's/.jsonl$//')
     COUNT=$(count-documents.py ${ID} | jq -r ".${ID}")
@@ -13,7 +46,6 @@ do
     fi
     if [ x${COUNT} = "x0" ]; then
         echo Posting ${FILE} to Solr ${ID}
-        < ${FILE} parallel -j 1 --files --pipe -l 65536 cat | parallel "post-all.py {} --core ${ID} --seq {#} --rename-id; rm {}; sleep 1"
+        < ${FILE} parallel -j 1 --files --pipe -l 65536 cat | parallel "post-simple.py {} --core ${ID} --seq {#} --rename-id; rm {}; sleep 1"
     fi
 done
-
