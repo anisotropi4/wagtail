7de06e11fe5755ba362929906e0516f0257028b1
Will Deakin <will.deakin@crinstitute.org.uk>
2020-02-03 17:17:33 +0000

    add solr running check and use post-simple command

--- WTT/run.sh
+++ WTT/run.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+export SOLRHOST=localhost
 export PYTHONUNBUFFERED=1
 export PATH=${PATH}:../bin
+
+count-documents.py _test_ 2> /dev/null
+if [ "$?" != "0" ]; then
+    start-cluster.sh
+    while true
+    do
+        count-documents.py _test_ 2> /dev/null
+        if [ "$?" = "0" ]; then
+            break
+        fi
+    done
+fi
 
 for ID in AA BS CR HD PA PATH TR ZZ
 do
@@ -11,6 +24,6 @@ do
         COUNT=0
     fi
     if [ x${COUNT} = "x0" ]; then
-        ls storage/${ID}_???.jsonl | parallel post-all.py {} --core ${ID} --default-fields NRS_Headcode
+        ls storage/${ID}_???.jsonl | parallel post-simple.py {} --core ${ID}
     fi
 done
