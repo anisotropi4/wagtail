15c2b544ead5ca888dc8145e218f3ce4b7b8089b
Guido Eco <will.deakin@crinstitute.org.uk>
2020-05-11 23:14:52 +0100

    update to create geolocation type

--- NaPTAN/run.sh
+++ NaPTAN/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 
 export PYTHONUNBUFFERED=1
 export PATH=${PATH}:../bin
@@ -27,7 +27,7 @@ if [ ! -s output/StopArea.jsonl ]; then
     xmltojson.py Naptan.xml --path output --depth 2
 fi
 
-echo Check docker cluster is running 
+echo Check docker cluster is running
 count-documents.py _test_ 2> /dev/null
 if [ "$?" != "0" ]; then
     start-cluster.sh
@@ -36,7 +36,7 @@ if [ "$?" != "0" ]; then
         echo
         echo NOTE: in the wagtail root directory to activate python virtual environment:
         echo $ source venv/bin/activate
-        echo 
+        echo
         exit 2
     fi
     while true
@@ -61,5 +61,6 @@ do
     if [ x${COUNT} = "x0" ]; then
         echo Posting ${FILE} to Solr ${ID}
         < ${FILE} parallel -j 1 --blocksize 8M --files --pipe -l 4096 cat | parallel "post-types.py {} --core ${ID} --seq {#} --rename-id; rm {}; sleep 1"
+        create-geo.py ${ID}
     fi
 done
