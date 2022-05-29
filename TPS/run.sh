c6aecf9fbb45198c0bd6686020807bc44c6b62cd
Will Deakin <will.deakin@crinstitute.org.uk>
2022-05-29 12:23:41 +0100

    update download filename and schema create

--- TPS/run.sh
+++ TPS/run.sh
@@ -3,6 +3,12 @@
 export PYTHONUNBUFFERED=1
 export PATH=${PATH}:../bin
 
+URL=https://networkrail.opendata.opentraintimes.com/mirror/tpsdata
+if [ ! -f file-list.txt ]; then
+    curl -k -s -L -G  ${URL} | html2json.py | \
+	jq -cr '.[]."File Name"' > file-list.txt
+fi
+
 count-documents.py _test_ 2> /dev/null
 if [ "$?" != "0" ]; then
     start-cluster.sh
@@ -15,17 +21,10 @@ if [ "$?" != "0" ]; then
     done
 fi
 
-URL=https://networkrail.opendata.opentraintimes.com/mirror/tpsdata
-if [ ! -f file-list.txt ]; then
-    curl -s -L -G  ${URL}| \
-        htmltojson.py --stdout --depth 5 | \
-        jq -cr '.td? | select(.[1] | tonumber > 1024)? | .[0].a.value' > file-list.txt
-fi
-
 if [ ! -f XML_p.xml ]; then
     FILENAME=$(tail -1 file-list.txt)
     echo Download and uncompress ${FILENAME} to TPS_Data.tar.bz2
-    curl ${URL}/${FILENAME} -o TPS_Data.tar.bz2
+    curl -k ${URL}/${FILENAME} -o TPS_Data.tar.bz2
     tar jxvf TPS_Data.tar.bz2
 fi
 
@@ -41,6 +40,7 @@ do
     COUNT=$(count-documents.py ${ID} | jq -r ".${ID}")
     echo ${ID} ${COUNT}
     if [ x${COUNT} = x"missing" ]; then
+	< ${FILE} parallel -j 1 --blocksize 32M --files --pipe -l 65536 cat | parallel "post-types.py {} --core ${ID} --seq {#} --set-schema --rename-id; rm {}; sleep 1"
         COUNT=0
     fi
     if [ x${COUNT} = "x0" ]; then
@@ -48,4 +48,3 @@ do
         < ${FILE} parallel -j 1 --blocksize 32M --files --pipe -l 65536 cat | parallel "post-types.py {} --core ${ID} --seq {#} --rename-id; rm {}; sleep 1"
     fi
 done
-
